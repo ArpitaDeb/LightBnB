596c6bfd284b50cc70984375e12cffa6cc6f7fa3
ArpitaDeb <arpitadeb1990@gmail.com>
2020-06-26 00:13:50 +0000

    getUserWithEmail getUserWithId addUser is working

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -18,7 +18,6 @@ const users = require('./json/users.json');
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
- */
 const getUserWithEmail = function(email) {
   let user;
   for (const userId in users) {
@@ -31,15 +30,30 @@ const getUserWithEmail = function(email) {
   }
   return Promise.resolve(user);
 }
-exports.getUserWithEmail = getUserWithEmail;
+*/
+const getUserWithEmail = function(email) {
+  return client.query(`
+  SELECT * FROM users WHERE email = $1;
+  `, [email])
+  .then(res => res.rows[0]);
+}
+ exports.getUserWithEmail = getUserWithEmail;
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
- */
+ 
 const getUserWithId = function(id) {
   return Promise.resolve(users[id]);
+}
+*/
+const getUserWithId = function(id) {
+  return client.query(`
+  SELECT * FROM users WHERE
+  id = $1;
+  `, [id])
+  .then(res => res.rows[0]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,12 +62,19 @@ exports.getUserWithId = getUserWithId;
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
- */
 const addUser =  function(user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
   return Promise.resolve(user);
+}
+*/
+const addUser =  function(user) {
+  return client.query(`
+  INSERT INTO users(name, password, email)
+  VALUES($1, $2, $3)
+  RETURNING *;`, [user.name, user.password, user.email])
+  .then(res => res.rows);
 }
 exports.addUser = addUser;
 
